800b144c900b29656edd46a06e54cf6a9ac3d6c5
Stanislav Kirichok <kirichok.stas@gmail.com>
2023-05-10 16:03:00 -0400

    Initial Commit

---
+++ SwiftUIEducation/SwiftUIEducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEducationApp.swift
+//  SwiftUIEducation
+//
+//  Created by Stas Kirichok on 10.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
